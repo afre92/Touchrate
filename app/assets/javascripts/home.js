573386aae896910c913701da9670f102172b2ef3
Andre <afre92@gmail.com>
2015-11-20 13:38:10 -0500

    methods placed in a better place

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,25 @@
+function drawChart(results) {
+      new Morris.Line({
+          element: 'month',
+          data: results,
+          xkey: '_id',
+          ykeys: ['t'],
+          labels: ['Engagements'],
+          lineColors: ['#009933'],
+          smooth: false
+
+    })
+};
+
+$.ajax({
+    url: 'https://touch-rate.com/o/analytics/sessions?api_key=d2e962d60f05511be209aafd7b2981f0&app_id=55e9994904161c2504fe29ca&period=30days',
+    dataType: 'jsonp',
+    success: function(results){
+        drawChart(results);
+      }
+  });
+
+
 $(document).ready(function(){
  var sessionsArr = new Array();
     // $.ajax({
@@ -9,61 +31,43 @@ $(document).ready(function(){
     //
     //
     // });
-    $.ajax({
-        url: 'https://touch-rate.com/o/analytics/sessions?api_k'
-        dataType: 'jsonp',
-        success: function(results){
-            drawChart(results);
-            console.log(results)
-          }
-      });
-
-    $.ajax({
-        url: "http://reports.touchrate.com/o?method=user_details"
-        dataType: 'jsonp',
-        success: function(results){
-          console.log(results)
-            var l = results["aaData"].length    /////////////////////////// SUPER WET CODE THAT I HAVE TO FIX///////////////////
-            var arr = results["aaData"]         // iterates through results to get sc(sessions count)of every store
-            for( var i = 0 ; i < l ; i++ ) {
-              var sc = arr[i].sc
-              sessionsArr.push(sc)
-            };
-            var largest = Math.max.apply(Math, sessionsArr);   // calculte the max of sc
-            for(var j = 0 ; j < l ; j++ ){                     // finds a store with the max sc and resturns it... What if i get two stores?
-              if (arr[j].sc === largest ) {
-                var storeMaxSessions = arr[j].name
-              }
-            };
-           maxValues(storeMaxSessions, largest);
-          }
-      });
 
 
+    // $.ajax({
+    //     url: "http://reports.touchrate.com/o?method=user_details"
+    //     dataType: 'jsonp',
+    //     success: function(results){
+    //       console.log(results)
+    //         var l = results["aaData"].length    /////////////////////////// SUPER WET CODE THAT I HAVE TO FIX///////////////////
+    //         var arr = results["aaData"]         // iterates through results to get sc(sessions count)of every store
+    //         for( var i = 0 ; i < l ; i++ ) {
+    //           var sc = arr[i].sc
+    //           sessionsArr.push(sc)
+    //         };
+    //         var largest = Math.max.apply(Math, sessionsArr);   // calculte the max of sc
+    //         for(var j = 0 ; j < l ; j++ ){                     // finds a store with the max sc and resturns it... What if i get two stores?
+    //           if (arr[j].sc === largest ) {
+    //             var storeMaxSessions = arr[j].name
+    //           }
+    //         };
+    //        maxValues(storeMaxSessions, largest);
+    //       }
+    //   });
+    //
+    //
+    //
+    // function maxValues(maxstore, maxsessions){
+    //   $(".maxStore").append(maxstore)
+    //   $(".maxSessions").append(maxsessions)
+    // };
+    //
+    //
+    //
+    // function passValues(value){
+    //     $(".numberOfStores").append(value["7days"]["dashboard"]["total_users"]["total"]),
+    //     $(".numberOfEngagements").append(value["7days"]["dashboard"]["total_sessions"]["total"])
+    //
+    //   };
 
-    function maxValues(maxstore, maxsessions){
-      $(".maxStore").append(maxstore)
-      $(".maxSessions").append(maxsessions)
-    };
 
-
-
-    function passValues(value){
-        $(".numberOfStores").append(value["7days"]["dashboard"]["total_users"]["total"]),
-        $(".numberOfEngagements").append(value["7days"]["dashboard"]["total_sessions"]["total"])
-
-      };
-
-    function drawChart(results) {
-          new Morris.Line({
-              element: 'week',
-              data: results,
-              xkey: '_id',
-              ykeys: ['t'],
-              labels: ['Engagements'],
-              lineColors: ['#009933'],
-              smooth: false
-
-        })
-    };
 });
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,130 @@
 module ApplicationHelper
+    require "net/https"
+    require "uri"
+    require "json"
+    require "date"
+    require 'jsonpath'
+
+   def topCatandSub
+     uri = URI.parse('https://touch-rate.com/o?method=events&api_key='+ENV["API_KEY"]+'&app_id='+ENV["APP_ID"]+'&event=Categories&segmentation=title&period=30days')
+     http = Net::HTTP.new(uri.host, uri.port)
+     http.use_ssl = true
+     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+     request = Net::HTTP::Get.new(uri.request_uri)
+     resp = http.request(request)
+     results = JSON.parse(resp.body)
+     data = results["2015"]
+     title = results["meta"]["title"]
+     algo = data.slice(*title)
+     algo2 = algo.sort_by { |k,v| v["c"] }
+     algo2[-1][0].gsub("\\n"," ").gsub("Games"," ")
+
+   end
+
+
+   def overallData(tiempo, kind)
+      uri = URI.parse('https://touch-rate.com/o/analytics/dashboard?api_key='+ENV["API_KEY"]+'&app_id='+ENV["APP_ID"])
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Get.new(uri.request_uri)
+      resp = http.request(request)
+      results = JSON.parse(resp.body)
+      results[tiempo]["dashboard"][kind]["total"]
+   end
+
+
+     def maxStore(arg1)
+       storeName = nil
+       arr = []
+       uri = URI.parse('https://touch-rate.com/o?method=user_details&api_key='+ENV["API_KEY"]+'&app_id='+ENV["APP_ID"])
+       http = Net::HTTP.new(uri.host, uri.port)
+       http.use_ssl = true
+       http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+       request = Net::HTTP::Get.new(uri.request_uri)
+       resp = http.request(request)
+       results = JSON.parse(resp.body)
+         results["aaData"].each do |n|
+            arr.push(n["sc"])
+          end
+           arr.max
+            if arg1 == "storeName"
+               results["aaData"].each do |u|
+                 if u["sc"] == arr.max
+                    return storeName = u["name"]
+                 end
+               end
+           end
+      end
+
+    def dayMaxEng
+      d = nil
+      arr = []
+      uri = URI.parse('https://touch-rate.com/o/analytics/sessions?api_key='+ENV["API_KEY"]+'&app_id='+ENV["APP_ID"]+'&period=30days')
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Get.new(uri.request_uri)
+      resp = http.request(request)
+      results = JSON.parse(resp.body)
+         results.each do |n|
+         arr.push(n["t"])
+         end
+       arr.max
+         results.each do |n|
+           if n["t"] == arr.max
+             maxDate = n["_id"]
+             d = Date.parse(maxDate)
+             return d.strftime('%A')
+          end
+        end
+
+     end
+
+      def categories(int)
+         vow = nil
+         arr = []
+         uri = URI.parse('https://touch-rate.com/o?method=events&api_key='+ENV["API_KEY"]+'&app_id='+ENV["APP_ID"]+'&event=Categories&segmentation=title&period=30days')
+         http = Net::HTTP.new(uri.host, uri.port)
+         http.use_ssl = true
+         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+         request = Net::HTTP::Get.new(uri.request_uri)
+         resp = http.request(request)
+         results = JSON.parse(resp.body)
+         data = results["2015"]
+         title = results["meta"]["title"]
+         algo = data.slice(*title).to_a
+           if algo[int] == nil
+             return "No Data"
+           else
+             return  vow = algo[int][0].gsub("\\nGames"," ").gsub("Games\\n"," ")
+           end
+      end
+
+        def percentCategories(int)
+         arr = []
+         uri = URI.parse('https://touch-rate.com/o?method=events&api_key='+ENV["API_KEY"]+'&app_id='+ENV["APP_ID"]+'&event=Categories&segmentation=title&period=30days')
+         http = Net::HTTP.new(uri.host, uri.port)
+         http.use_ssl = true
+         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+         request = Net::HTTP::Get.new(uri.request_uri)
+         resp = http.request(request)
+         results = JSON.parse(resp.body)
+         data = results["2015"]
+         title = results["meta"]["title"]
+         algo = data.slice(*title).to_a
+           if algo[int] == nil
+                return 0
+             else
+               algo[int][0].gsub("\\n"," ")
+                 algo.each do |a|
+                 arr.push(a[1]["c"])
+                end
+              return algo[int][1]["c"]*100 / arr.sum
+            end
+        end
+
+
+
+
 end
